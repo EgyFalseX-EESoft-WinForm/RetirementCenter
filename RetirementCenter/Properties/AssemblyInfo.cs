97fb28fd99823b903df525d9cbef4ffb48fbead0
mohamed.aly.omer@gmail.com <Mohamed!983>
2019-06-15 17:01:52 +0200

    Change Expire date

--- RetirementCenter/Properties/AssemblyInfo.cs
+++ RetirementCenter/Properties/AssemblyInfo.cs
@@ -26,5 +26,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.681")] 
-[assembly: AssemblyFileVersion("1.0.0.681")]
+[assembly: AssemblyVersion("1.0.0.682")] 
+[assembly: AssemblyFileVersion("1.0.0.682")]
