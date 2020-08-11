c8666f9f1e7a3c902329cbcb2f0ab4482b38c675
mohamed aly omar <hmd3a@hotmail.com>
2020-08-11 20:51:31 +0200

    change version

--- RetirementCenter/Properties/AssemblyInfo.cs
+++ RetirementCenter/Properties/AssemblyInfo.cs
@@ -26,5 +26,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.693")] 
-[assembly: AssemblyFileVersion("1.0.0.693")]
+[assembly: AssemblyVersion("1.0.0.695")] 
+[assembly: AssemblyFileVersion("1.0.0.695")]
